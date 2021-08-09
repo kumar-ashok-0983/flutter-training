6cbc39458defcc50865b023d7afea6310eb001e9
Tiago <tiagobencardino@gmail.com>
2021-08-09 17:44:33 +0100

    add mock json on the new screen

--- playground/flutter_hello_world/lib/main.dart
+++ playground/flutter_hello_world/lib/main.dart
@@ -24,8 +24,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.amber,
       ),
-      // home: NewScreen(),
-      home: TiagoHomePage(),
+      home: NewScreen(),
+      // home: TiagoHomePage(),
     );
   }
 }
